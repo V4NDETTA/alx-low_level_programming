59a28e9814ca02b57efc405a1e08bc2e4c91c84a
V4NDETTA <bilalbelamammar@gmail.com>
2023-03-20 10:31:48 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a' ; alph <= 'z' ; alph++)
+	{
+		if (alph != 'e' && alph != 'q')
+		{
+			putchar(alph);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
