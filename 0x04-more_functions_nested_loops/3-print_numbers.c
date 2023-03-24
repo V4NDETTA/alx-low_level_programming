2a0d069917d0d519c743ae57536c5459ac238016
V4NDETTA <bilalbelamammar@gmail.com>
2023-03-24 14:03:53 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_numbers - print numbers 0 to 9 followed by new line
+  *
+  * Return: Void.
+  */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar(10);
+}
